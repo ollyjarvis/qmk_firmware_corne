2e0a8feaaeb1a4d668b00bc3537e64422af98cfe
Oliver Jarvis <olly.m.jarvis@gmail.com>
2024-08-25 12:30:59 +0100

    okurr

---
+++ keyboards/crkbd/keymaps/ollyjarvis/layers.h
@@ -0,0 +1,35 @@
+/***************************************************************************************************************\
+* Layer name declarations.                                                                                      *
+*                                                                                                               *
+* Copyright 2021-2022  Leandro Emmanuel Reina Kiperman <@kip93>                                                 *
+*                                                                                                               *
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General    *
+* Public License as published by the Free Software Foundation, either version 3 of the License, or (at your     *
+* option) any later version.                                                                                    *
+*                                                                                                               *
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the    *
+* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   *
+* for more details.                                                                                             *
+*                                                                                                               *
+* You should have received a copy of the GNU General Public License along with this program. If not, see        *
+* <http://www.gnu.org/licenses/>.                                                                               *
+\***************************************************************************************************************/
+
+#pragma once
+
+////////////////////////////////////////////// Layer declarations ///////////////////////////////////////////////
+
+enum {
+    TYPING_0 = 0,
+    TYPING_1,
+    TYPING_2,
+};
+
+//////////////////////////////////////////////////// Aliases ////////////////////////////////////////////////////
+// Easier to put in the keymap
+
+enum {
+    T0 = TYPING_0,
+    T1 = TYPING_1,
+    T2 = TYPING_2,
+};
